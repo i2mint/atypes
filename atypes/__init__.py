9e7ebbb501a1f4d4e16de7473d562c8a287c2905
Thor Whalen <thorwhalen1@gmail.com>
2021-09-28 17:13:56 -0700

    feat: Gettable

--- atypes/__init__.py
+++ atypes/__init__.py
@@ -3,8 +3,16 @@
 
 from atypes.util import MyType
 from atypes.typ import (
-    Waveform, Waveforms, KeyWfGen, Chunk, Chunks, Chunker, Feature, FV, FVs,
-    Featurizer, ChkFeaturizer
+    Waveform,
+    Waveforms,
+    KeyWfGen,
+    Chunk,
+    Chunks,
+    Chunker,
+    Feature,
+    FV,
+    FVs,
+    Featurizer,
+    ChkFeaturizer,
 )
-
-
+from atypes.protocols import Gettable
