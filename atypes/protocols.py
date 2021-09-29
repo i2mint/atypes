ba6d49e1c7bbf302723b39e156dad13931a169a4
Thor Whalen <thorwhalen1@gmail.com>
2021-09-28 17:17:55 -0700

    fix: commented out breaking code

--- atypes/protocols.py
+++ atypes/protocols.py
@@ -95,73 +95,22 @@ class ChkFeaturizer(Protocol):
 import typing as ty
 import collections.abc
 
-
-class SizableIterable(collections.abc.Sized, collections.abc.Iterable):
-    pass
-
-
-class SizableIterable2(ty.Protocol):
-    __len__: Callable
-    __iter__: Callable
-
-
-# return
-# [[1, 2, 3], [3, 4, 5], [5, 6, 7]]  # Iterable[SizableIterable[T]]
-
-T = ty.TypeVar('T')
-
-
-class Chunker(ty.Protocol):
-    def __call__(self, x: Iterable[T]) -> Iterable[SizableIterable[T]]:
-        ...
-
-from atypes.typ import Waveform, Chunks
-
-WfChunker = Callable[[Waveform], Chunks]
-
-foo: WfChunker
-
-
-# linter (unexpectedly) DOES NOT COMPLAIN
-def foo(wf) -> int:
-    return 3
-
-
-def bar(wf_chunker: WfChunker):
-    return 42
-
-
-bar(wf_chunker=3)  # linter complains (wf_chunker not Callable)
-bar(wf_chunker=lambda wf: 3)  # linter complains (wf_chunker doesn't return iterable)
-bar(wf_chunker=lambda wf: [1, 2, 3])  # linter complains
-bar(wf_chunker=lambda wf: [[1, 2, 3], [4, 5]])  # linter (unexpectedly) complains
-bar(wf_chunker=lambda wf: None)  # linter (unexpectedly) DOES NOT complain
-
-
-def not_a_chunker(wf):
-    return 3
-
-
-bar(wf_chunker=not_a_chunker)  # linter complains
-
-
-def chunker(wf):
-    return [[1, 2, 3], [3, 4, 5]]
-
-
-bar(wf_chunker=chunker)  # linter (unexpectedly) complains
-
-
-def annotated_chunker(wf: Waveform) -> Chunks:
-    return [[1, 2, 3], [3, 4, 5]]
-
-
-bar(wf_chunker=annotated_chunker)  # linter (unexpectedly) complains
-
-
-def still_not_a_chunker(wf):
-    return 3
-
-
-still_not_a_chunker: WfChunker
-bar(wf_chunker=still_not_a_chunker)  # linter (unexpectedly) DOES NOT COMPLAIN
+# TODO: Make this work
+# class SizableIterable(collections.abc.Sized, collections.abc.Iterable):
+#     pass
+#
+#
+# class SizableIterable2(ty.Protocol):
+#     __len__: Callable
+#     __iter__: Callable
+#
+#
+# # return
+# # [[1, 2, 3], [3, 4, 5], [5, 6, 7]]  # Iterable[SizableIterable[T]]
+#
+# T = ty.TypeVar('T')
+#
+#
+# class Chunker(ty.Protocol):
+#     def __call__(self, x: Iterable[T]) -> Iterable[SizableIterable[T]]:
+#         ...
